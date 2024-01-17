73b190c6cff2141dca37c6ac9fddc5fc9e3d658c
TimothySherDev <80216575+TimothySherDev@users.noreply.github.com>
2024-01-17 16:28:16 -0500

    Fixed Act transitions

--- Assets/Scripting/Scripts/Game Manager/Act1EndTranAct2.cs
+++ Assets/Scripting/Scripts/Game Manager/Act1EndTranAct2.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Act1EndTranAct2 : MonoBehaviour
 {
     public int SceneBuildIndex;
+    playerHealth  playerCheck;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player") {
+
+
+        playerCheck = GameObject.Find("Player").GetComponent<playerHealth>();
+
+        if (other.tag == "Player" && playerCheck.currentHP == 0) {
             SceneManager.LoadScene(SceneBuildIndex, LoadSceneMode.Single);
         
         }
