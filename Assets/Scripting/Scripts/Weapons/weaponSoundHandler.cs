d1ffbc994a047a2c287c37b078386522e040d429
yuri3330 <yuri3330@hotmail.ca>
2024-02-21 14:23:59 -0500

    pistol alt charge removed from original script

--- Assets/Scripting/Scripts/Weapons/weaponSoundHandler.cs
+++ Assets/Scripting/Scripts/Weapons/weaponSoundHandler.cs
@@ -39,7 +39,7 @@ public class weaponSoundHandler : MonoBehaviour
         switch (weaponType)
         {
             case weaponType.revolver:
-                AudioManager.instance.PlaySFX(FMODEvents.instance.chargePistolShot, this.transform.position);
+                //AudioManager.instance.PlaySFX(FMODEvents.instance.chargePistolShot, this.transform.position);
                 break;
             case weaponType.shotgun:
                 AudioManager.instance.PlaySFX(FMODEvents.instance.altShotgunShot, this.transform.position);
