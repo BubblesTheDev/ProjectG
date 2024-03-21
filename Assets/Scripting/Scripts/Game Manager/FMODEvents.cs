2d2f3c7e0484dfecfcc1249282682d8a9bc6e22c
yuri3330 <yuri3330@hotmail.ca>
2024-03-21 03:20:38 -0400

    Seeker SFX in game

--- Assets/Scripting/Scripts/Game Manager/FMODEvents.cs
+++ Assets/Scripting/Scripts/Game Manager/FMODEvents.cs
@@ -31,6 +31,9 @@ public class FMODEvents : MonoBehaviour
     [field: SerializeField] public EventReference turretCharge { get; private set; }
     [field: SerializeField] public EventReference turretShoot { get; private set; }
     [field: SerializeField] public EventReference cerberusShoot { get; private set; }
+    [field: SerializeField] public EventReference seekerDash { get; private set; }
+    [field: SerializeField] public EventReference seekerDashStart { get; private set; }
+    [field: SerializeField] public EventReference seekerSlash { get; private set; }
 
     [field: Header("Player SFX")]
     [field: SerializeField] public EventReference jumpSFX { get; private set; }
