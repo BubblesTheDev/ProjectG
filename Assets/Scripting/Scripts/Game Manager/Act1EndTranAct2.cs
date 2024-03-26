f66d55e61e80b3abe3381840b4606ddfa4338242
BubblesTheDev <78316409+BubblesTheDev@users.noreply.github.com>
2024-03-26 14:48:01 -0400

    Update Act1EndTranAct2.cs

--- Assets/Scripting/Scripts/Game Manager/Act1EndTranAct2.cs
+++ Assets/Scripting/Scripts/Game Manager/Act1EndTranAct2.cs
@@ -1,23 +1,15 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
-//using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Act1EndTranAct2 : MonoBehaviour
 {
     public int SceneBuildIndex;
-    playerHealth  playerCheck;
-
     private void OnTriggerEnter(Collider other)
     {
-
-        playerCheck = GameObject.Find("Player").GetComponent<playerHealth>();
-
-        if (other.tag == "Player" && playerCheck.currentHP <= 0) {
+        if (other.CompareTag("Player")) {
             SceneManager.LoadScene(SceneBuildIndex, LoadSceneMode.Single);
-        
         }
     }
 }
-*/
