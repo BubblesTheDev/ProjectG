98a5305c20b1ff7b0b96c94f2ec2f5793f800750
BubblesTheDev <78316409+BubblesTheDev@users.noreply.github.com>
2024-04-15 18:40:59 -0400

    Added Death jingle script

---
+++ Assets/Scripting/Scripts/Game Manager/playDeathJingle.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playDeathJingle : MonoBehaviour
+{
+    private void Awake()
+    {
+        //Play death jingle here
+    }
+}
