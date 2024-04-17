8154fc4cf0afd5b5ac0c11cd020cc76210fb19de
ginger-aile <gingeraile123321@gmail.com>
2024-04-17 15:46:02 -0400

    end cutscene is in

--- Assets/Scripting/Scripts/UI/CutscenePrompt.cs
+++ Assets/Scripting/Scripts/UI/CutscenePrompt.cs
@@ -6,6 +6,7 @@ using TMPro;
 public class CutscenePrompt : MonoBehaviour
 {
     [SerializeField] private NewBehaviourScript timerScript;
+    private bool startFadeIn = false;
     // Start is called before the first frame update
     void Awake()
     {
@@ -15,17 +16,22 @@ public class CutscenePrompt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (timerScript.timerRemaining <= 0)
+        if (enabled && startFadeIn)
         {
-            FadeInText();
-            gameObject.GetComponent<TextMeshProUGUI>().color = Color.Lerp(Color.white, new Color(255, 255, 255, 0), Mathf.PingPong(Time.time, 1));
+            gameObject.GetComponent<TextMeshProUGUI>().color = Color.Lerp(new Color(255, 255, 255, 0), Color.white, Mathf.PingPong(Time.time, 1));
         }
         else gameObject.GetComponent<TextMeshProUGUI>().color = Color.clear;
     }
 
+    private void OnEnable()
+    {
+        StartCoroutine(FadeInText());
+    }
+
     private IEnumerator FadeInText()
     {
+        yield return new WaitForSeconds(2f);
         gameObject.GetComponent<TextMeshProUGUI>().color = Color.Lerp(new Color(255, 255, 255, 0), Color.white, 0.5f);
-        return null;
+        startFadeIn = true;
     }
 }
