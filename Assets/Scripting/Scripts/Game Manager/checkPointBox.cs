2a30fbb967cab9d6d8af65f012a0b276b30b9f57
ginger-aile <gingeraile123321@gmail.com>
2024-04-17 18:02:22 -0400

    checkpoint vignette

--- Assets/Scripting/Scripts/Game Manager/checkPointBox.cs
+++ Assets/Scripting/Scripts/Game Manager/checkPointBox.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class checkPointBox : MonoBehaviour
 {
     [SerializeField] private int checkpointIndex;
+    [SerializeField] private Volume checkpointPPR;
+    [SerializeField] private float checkpointVignetteShowTime;
+
     checkpointSystem brain;
     private bool hasGottenCheckpoint;
     
@@ -21,6 +25,30 @@ public class checkPointBox : MonoBehaviour
             hasGottenCheckpoint = true;
             brain.updateCheckpoint(checkpointIndex);
             StartCoroutine(brain.getCheckpoint());
+            StartCoroutine(CheckpointVignette());
         }
+    }
+
+    private IEnumerator CheckpointVignette()
+    {
+        float timer = 0;
+        while (timer < checkpointVignetteShowTime / 3)
+        {
+            timer += Time.deltaTime;
+            checkpointPPR.weight = timer / (checkpointVignetteShowTime / 3);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(checkpointVignetteShowTime / 3);
+        timer = 0;
+        while (timer < checkpointVignetteShowTime / 3)
+        {
+            timer += Time.deltaTime;
+            checkpointPPR.weight = 1 - (timer / (checkpointVignetteShowTime / 3));
+            yield return null;
+        }
+
+        checkpointPPR.weight = 0;
+
     }
 }
